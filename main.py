a3b059db971ddd66873825dffd325c631f223ba0
Nicolas <nicolasthiry5@gmail.com>
2021-12-05 13:01:30 +0200

    Creation of the deduction function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def add (number1, number2):
-    #function that add 2 numbers together and return the answer
+    #function that adds 2 numbers together and return the answer
     return number1 + number2
 
+def deduction (number1, number2):
+    #function that substracts 2 numbers together and return the answer
+    return number1 - number2
 
